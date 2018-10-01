cef92efcb575710a1994a5bb57095cc7af2d0250
NyanKiyoshi <6186720+NyanKiyoshi@users.noreply.github.com>
2018-10-02 00:43:12 +0200

    Attempt to test things through Travis

---
+++ tests/Assets/Editor/Tests/CoreTests.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using ngettext_unity;
+
+public class CoreTests {
+
+    [Test]
+    public void CoreTestsSimplePasses() {
+        // Use the Assert class to test conditions.
+        NGettextUnity.Instance.LoadLocale("fr-FR");
+        Assert.AreEqual(
+            NGettextUnity.Catalog.GetString("Hello, {0}!", "World"), "Heyo, World!");
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator CoreTestsWithEnumeratorPasses() {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        yield return null;
+    }
+}
---
+++ tests/Assets/Scenes/MainSceneManager.cs
@@ -0,0 +1,18 @@
+﻿using ngettext_unity;
+using UnityEngine;
+
+public class MainScene : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    	
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
